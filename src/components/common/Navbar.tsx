897122f2d65beca2d96b1191b93f7e9032c71b8b
Sejan~ <md4sejan@gmail.com>
2024-10-27 03:46:33 +0600

    navbar update

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,19 +2,18 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { usePathname } from "next/navigation"; // Import usePathname to track the current route
+import { usePathname } from "next/navigation";
 
 const Navbar: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const pathname = usePathname(); // Get the current path
+  const pathname = usePathname();
 
-  // Function to determine if the link is active
   const isActive = (path: string) => pathname === path;
 
   return (
     <nav className="bg-white shadow sticky top-0 z-10">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-14">
+      <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
           {/* Logo or Title */}
           <div className="flex-shrink-0">
             <Link href="/">
@@ -74,13 +73,21 @@ const Navbar: React.FC = () => {
             </Link>
           </div>
 
+          <div>
+            <Link
+              href="/admin"
+              className="px-4 py-2 rounded-md text-white font-semibold bg-blue-600 hover:bg-gray-700 transition-colors duration-200"
+            >
+              Dashboard
+            </Link>
+          </div>
+
           <div className="flex md:hidden">
             <button
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
               className="p-2 text-gray-600 hover:text-gray-900"
               aria-label="Toggle Menu"
             >
-              {/* Hamburger Icon */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -105,7 +112,7 @@ const Navbar: React.FC = () => {
         <div className="flex flex-col space-y-2 p-4">
           <Link
             href="/"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            onClick={() => setIsMobileMenuOpen(false)}
             className={`text-gray-600 hover:text-gray-900 ${
               isActive("/") ? "text-blue-600 font-semibold" : ""
             }`}
@@ -114,7 +121,7 @@ const Navbar: React.FC = () => {
           </Link>
           <Link
             href="/product"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            onClick={() => setIsMobileMenuOpen(false)}
             className={`text-gray-600 hover:text-gray-900 ${
               isActive("/product") ? "text-blue-600 font-semibold" : ""
             }`}
@@ -123,7 +130,7 @@ const Navbar: React.FC = () => {
           </Link>
           <Link
             href="/cart"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            onClick={() => setIsMobileMenuOpen(false)}
             className={`text-gray-600 hover:text-gray-900 ${
               isActive("/cart") ? "text-blue-600 font-semibold" : ""
             }`}
@@ -132,7 +139,7 @@ const Navbar: React.FC = () => {
           </Link>
           <Link
             href="/checkout"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            onClick={() => setIsMobileMenuOpen(false)}
             className={`text-gray-600 hover:text-gray-900 ${
               isActive("/checkout") ? "text-blue-600 font-semibold" : ""
             }`}
@@ -141,7 +148,7 @@ const Navbar: React.FC = () => {
           </Link>
           <Link
             href="/orders"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            onClick={() => setIsMobileMenuOpen(false)}
             className={`text-gray-600 hover:text-gray-900 ${
               isActive("/orders") ? "text-blue-600 font-semibold" : ""
             }`}
@@ -150,7 +157,7 @@ const Navbar: React.FC = () => {
           </Link>
           <Link
             href="/contact"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            onClick={() => setIsMobileMenuOpen(false)}
             className={`text-gray-600 hover:text-gray-900 ${
               isActive("/contact") ? "text-blue-600 font-semibold" : ""
             }`}
